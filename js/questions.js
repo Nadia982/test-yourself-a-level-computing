b75d9833c7d507b77d47774e6e883c15c6382c10
Natalie Gillam <ngillam@hotmail.co.uk>
2024-11-17 23:50:19 +0000

    added more questions about 'thinking abstractly'

--- js/questions.js
+++ js/questions.js
@@ -1,10 +1,34 @@
 const questions = [
   {
     id: 1,
-    q: "map",
-    definition: "A drawing of the whole of, or part of, an area, showing its features",
+    q: "What are the four principles of computational thinking?",
+    definition: "1. Abstraction. 2. Decomposition. 3. Algorithmic thinking. 4. Pattern recognition.",
     options: ["yes", "no"],
     a: 0,  
-    category: "2.1 - Thinking Abstractly" 
-  }
+    category: "2.1.1 - Thinking Abstractly" 
+  }, 
+  {
+    id: 2,
+    q: "What does 'abstraction' mean?",
+    definition: "Removing unnecessary details and leaving in only the key details required to solve the problem.",
+    options: ["yes", "no"],
+    a: 0,  
+    category: "2.1.1 - Thinking Abstractly" 
+  }, 
+  {
+    id: 3,
+    q: "What does 'decomposition' mean?",
+    definition: "Breaking a complex problem down into smaller, more manageable parts.",
+    options: ["yes", "no"],
+    a: 0,  
+    category: "2.1.1 - Thinking Abstractly" 
+  }, 
+  {
+    id: 4,
+    q: "Give an example of 'abstraction' in real life?",
+    definition: "The tube map, maps in general, floor plans of a house, outline drawings used in icons e.g. the 'home' icon, fingerprint recognition, iris scanning, text recognition and number plate recognition.",
+    options: ["yes", "no"],
+    a: 0,  
+    category: "2.1.1 - Thinking Abstractly" 
+  }, 
 ]
