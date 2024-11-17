4d7f16a95ca989aed4fcb91727c7c43ad31fa5ec
Natalie Gillam <ngillam@hotmail.co.uk>
2024-11-17 02:50:32 +0000

    changed contents of title tag

--- js/questions.js
+++ js/questions.js
@@ -5,6 +5,6 @@ const questions = [
     definition: "A drawing of the whole of, or part of, an area, showing its features",
     options: ["yes", "no"],
     a: 0,  
-    category: "2.1" 
+    category: "2.1 - Thinking Abstractly" 
   }
 ]
